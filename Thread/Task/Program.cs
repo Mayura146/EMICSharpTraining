b7dc892b8bfab0c63137289107bdac15a843e32a
mayura.sonar <mayura.sonar@outlook.com>
2022-05-19 12:27:29 +0530

    Thread Demo

---
+++ Thread/Task/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Program
+    {
+        public static void PrintNumbers()
+        {
+            Console.WriteLine($"Child Thread: {Thread.CurrentThread.ManagedThreadId} Started");
+            for (int i = 0; i <=20;i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine($"Child Thread: {Thread.CurrentThread.ManagedThreadId} Completed");
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Main Thread :{Thread.CurrentThread?.ManagedThreadId} Started");
+            Task task = Task.Run(() =>
+            {
+                PrintNumbers();
+            });
+           task.Wait();
+            Console.WriteLine($"Main Thread :{Thread.CurrentThread?.ManagedThreadId} Completed");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Thread/Thread/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+namespace Thread1
+{
+    internal class Program
+    {
+     public void ThreadMethod1()
+        {
+          
+            Console.WriteLine("ThreadMethod1 Executed" +Thread.CurrentThread.Name);
+        
+        }
+        public void ThreadMethod2()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                  Thread.Sleep(5000);
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("ThreadMethod2 Executed" + Thread.CurrentThread.Name);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Main Method Entered");
+            Program p=new Program();
+            Thread t1 = new Thread(p.ThreadMethod1);
+            Thread t2 = new Thread(p.ThreadMethod2);
+            //      Thread t3=new Thread()
+            t1.Name = "Demo";
+            t2.Start();
+          t2.Join();
+            // Threadpool
+            t1.Start();
+    
+             
+        
+
+            Console.WriteLine("Main Method Executed");
+
+        }
+    }
+}
+
