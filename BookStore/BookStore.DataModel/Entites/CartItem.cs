127d6f496ac79a5f17c9267afe185e754e61e612
Mayura Sonar <Mayura.Sonar@euromonitor.com>
2022-06-01 15:26:51 +0530

    fsdf

---
+++ BookStore/BookStore.DataModel/Entites/CartItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace BookStore.DataModel.Entites
+{
+    public partial class CartItem
+    {
+        public int CartItemId { get; set; }
+        public string CartId { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
